c7454375c33c3e695075cdf562a375d50d7cba76
Jordan Cotton <Jordancotton55@gmail.com>
2017-05-15 01:42:21 +0300

    Add user search and display functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const clientID = '';
+const clientID = 'lr4rv2eagssyln2lhrm19l8jrjn1mc';
 const twitchChannels = [
         {username: 'esl_sc2', id: '30220059'},
         {username: 'OgamingSC2', id: '71852806'},
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.log(data);
                 let row = document.getElementById(channelRows[channels].id);
                 if (data.stream != null) {
-                    row.innerHTML = '<div></div>' + data.stream.channel.display_name  + ' | ' + data.stream.game + ' | <a href="' + data.stream.channel.url + '">Go now!</a>';
+                    row.innerHTML = '<div></div>' + data.stream.channel.display_name  + ' | ' + data.stream.game + ' | <a href="' + data.stream.channel.url + '" target="_blank">Go now!</a>';
                     row.firstElementChild.classList.add('is-online');
                 } else {
                     row.innerHTML = '<div></div>' + twitchChannels[channels].username + ' is not online.';
@@ -38,3 +38,58 @@ document.addEventListener("DOMContentLoaded", () => {
 	httpRequest.send(); 
     };
 });
+
+$('#submit-button').click(() => {
+    findTwitchUser($('#user-input').val());
+});
+
+function findTwitchUser(user) {
+    let httpRequest = new XMLHttpRequest();
+
+    httpRequest.open('GET', 'https://api.twitch.tv/kraken/users?login=' + user);
+
+    httpRequest.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+    httpRequest.setRequestHeader('Client-ID', clientID);
+
+    httpRequest.onload = function()  {
+        if (this.readyState === 4) {
+            let data = JSON.parse(this.responseText);
+            if (data._total === 0) {
+                alert('User not found, sorry!');
+            } else {
+              addTwitchUser(data.users[0].display_name, data.users[0]._id);  
+            }
+            
+        }
+    }
+    httpRequest.send();
+}
+
+
+function addTwitchUser(username, userID) {
+    let rowCounter = 4;
+    let nextRequest = new XMLHttpRequest();
+
+    nextRequest.open('GET', 'https://api.twitch.tv/kraken/streams/' + userID, true);
+
+    nextRequest.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+    nextRequest.setRequestHeader('Client-ID', clientID);
+
+    nextRequest.onload = function() {
+        if (this.readyState === 4) {
+            let appView = document.getElementById('app-view');
+            let data = JSON.parse(this.responseText);
+            console.log(data);
+            
+            if (data.stream != null) {
+                $('#app-view').append('<div class="twitch-user-row"><div></div>' + data.stream.channel.display_name  + ' | ' + data.stream.game + ' | <a href="' + data.stream.channel.url + '" target="_blank">Go now!</a></div>');
+                appView.lastChild.firstElementChild.classList.add('is-online');
+            } else {
+                $('#app-view').append('<div class="twitch-user-row"><div></div>' + username + ' is not online.');
+                appView.lastChild.firstElementChild.classList.add('is-offline');
+            }
+            rowCounter++;
+        }
+    }
+    nextRequest.send();
+}
